4f4b8c5856edf709d712c2e6467a65ca4fe0e71c
gurdeepk <gurdeepk@lentra.ai>
2025-04-20 11:22:42 +0530

    Initial commit

---
+++ src/main/java/com/demo/sub_probe/SubProbeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sub_probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubProbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubProbeApplication.class, args);
+	}
+
+}
